4b36746135f6274ccbea65d8c6d620d3cfac1c6a
Josep Borrell-Tatche <jborrell@tikgpu02.ee.ethz.ch>
2024-11-27 18:14:56 +0100

    chore: get right data to obtain bd rate

---
+++ scripts/copy_new_results.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import shutil
+import datetime
+
+
+def dirs_to_copy(exp_path: Path):
+    all_dirs = []
+
+    for subdir in exp_path.iterdir():
+        if not subdir.is_dir():
+            continue
+        for subsubdir in subdir.iterdir():
+            # Get the creation time
+            creation_time = subsubdir.stat().st_ctime
+            creation_date = datetime.datetime.fromtimestamp(creation_time)
+            if creation_date > datetime.datetime.fromisoformat("2024-11-24"):
+                all_dirs.append(subsubdir)
+    return all_dirs
+
+
+def copy_dir(origin: Path, destination: Path):
+    shutil.copytree(origin, destination)
+
+
+if __name__ == "__main__":
+    exp_path = Path("results/exps/2024-11-21/")
+    dest_dir = Path("results/exps/2024-11-26/")
+    dirs = dirs_to_copy(exp_path)
+    for dir in dirs:
+        copy_dir(dir, dest_dir / dir.relative_to(exp_path))
