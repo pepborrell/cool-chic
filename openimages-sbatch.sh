a5b409203c96f8b5c82972caee5926ade943129b
Josep Borrell-Tatche <jborrell@tik42x.ee.ethz.ch>
2025-01-13 19:02:08 +0100

    feat: leftover last changes from training in the server

--- openimages-sbatch.sh
+++ openimages-sbatch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for (( i=0; i<=100; i++ )); do
-	job_number=$(printf "%02d" $i)  # Format the number as two digits
-	echo "Launching job $job_number"
+for (( i=101; i<=1000; i++ )); do
+	if ((i % 100 == 0)); then
+		job_number=$(printf "%02d" $i)  # Format the number as two digits
+		echo "Launching job $job_number"
+	fi
 	sbatch submit-simpler-enc.sh cfg/exps/openimages/encode.yaml $i
 done
