fix: latents were always parameters before and restricted gradient flow to analysis transform. fixed it with the right hypernet approach.

coolchic/enc/component/coolchic.py
@@ -141,6 +141,16 @@ class CoolChicEncoderOutput:
     additional_data: dict[str, Any]
 
 
+class CoolChicLatentGrid(nn.Module):
+    """Stores the latent grid for a single resolution.
+    Necessary, because we can't directly change tensors in a parameter list, but we can from modules.
+    """
+
+    def __init__(self, data: torch.Tensor):
+        super().__init__()
+        self.data = nn.Parameter(data, requires_grad=True)
+
+
 class CoolChicEncoder(nn.Module):
     """CoolChicEncoder for a single frame."""
 
@@ -170,7 +180,7 @@ class CoolChicEncoder(nn.Module):
 
         # Populate the successive grids
         self.size_per_latent = []
-        self.latent_grids = nn.ParameterList()
+        self.latent_grids = nn.ModuleList()
         for i in range(self.param.latent_n_grids):
             h_grid, w_grid = [int(math.ceil(x / (2**i))) for x in self.param.img_size]
             c_grid = self.param.n_ft_per_res[i]
@@ -180,9 +190,7 @@ class CoolChicEncoder(nn.Module):
 
             # Instantiate empty tensor, we fill them later on with the function
             # self.initialize_latent_grids()
-            self.latent_grids.append(
-                nn.Parameter(torch.empty(cur_size), requires_grad=True)
-            )
+            self.latent_grids.append(CoolChicLatentGrid(torch.empty(cur_size)))
 
         self.initialize_latent_grids()
 
@@ -350,7 +358,7 @@ class CoolChicEncoder(nn.Module):
         # to a single flat vector. This allows to call the quantization
         # only once, which is faster
         encoder_side_flat_latent = torch.cat(
-            [latent_i.view(-1) for latent_i in self.latent_grids]
+            [latent_i.data.view(-1) for latent_i in self.latent_grids]
         )
 
         flat_decoder_side_latent = quantize(
@@ -502,12 +510,11 @@ class CoolChicEncoder(nn.Module):
                 "random_seed is None."
             )
             generator = torch.Generator().manual_seed(random_seed)
-        for latent_index, latent_value in enumerate(self.latent_grids):
-            self.latent_grids[latent_index] = nn.Parameter(
-                torch.zeros_like(latent_value)
+        for latent_index, latent in enumerate(self.latent_grids):
+            self.latent_grids[latent_index] = CoolChicLatentGrid(
+                torch.zeros_like(latent.data)
                 if zeros
-                else 1e-2 * torch.randn(latent_value.shape, generator=generator),  # pyright: ignore
-                requires_grad=True,
+                else 1e-2 * torch.randn(latent.shape, generator=generator),  # pyright: ignore
             )
 
     def reinitialize_parameters(self):

coolchic/hypernet/hypernet.py
@@ -534,7 +534,12 @@ class CoolchicWholeNet(WholeNet):
         self.cc_encoder.size_per_latent = [
             (1, *lat.shape[-3:]) for lat in latent_weights
         ]
-        self.cc_encoder.latent_grids = nn.ParameterList(latent_weights)
+        # Something like self.cc_encoder.latent_grids = nn.ParameterList(latent_weights)
+        # would break the computation graph. This doesn't. Following tips in:
+        # https://github.com/qu-gg/torch-hypernetwork-tutorials?tab=readme-ov-file#tensorVSparameter
+        for i in range(len(self.latent_grids)):
+            del self.latent_grids[i].data
+            self.latent_grids[i].data = latent_weights[i]
 
         return self.cc_encoder
 
@@ -597,7 +602,13 @@ class LatentDecoder(CoolChicEncoder):
     ) -> tuple[torch.Tensor, torch.Tensor, dict[str, Any]]:
         # Replace latents in CoolChicEncoder.
         self.size_per_latent = [(1, *lat.shape[-3:]) for lat in latents]
-        self.latent_grids = nn.ParameterList(latents)
+        # Something like self.latent_grids = nn.ParameterList(latents)
+        # would break the computation graph. This doesn't. Following tips in:
+        # https://github.com/qu-gg/torch-hypernetwork-tutorials?tab=readme-ov-file#tensorVSparameter
+        for i in range(len(self.latent_grids)):
+            del self.latent_grids[i].data
+            self.latent_grids[i].data = latents[i]
+
         # This makes synthesis and arm happen with deltas added to the filters.
         if synth_delta is not None:
             self.synthesis.add_delta(synth_delta)
@@ -640,6 +651,26 @@ class NOWholeNet(WholeNet):
         noise_parameter: float = 0.25,
     ) -> tuple[torch.Tensor, torch.Tensor, dict[str, Any]]:
         latents = self.encoder.forward(img)
+        # # Function to display tensors
+        # def show_tensors(tensors: list[torch.Tensor]):
+        #     dpi = 200
+        #     fig, axes = plt.subplots(
+        #         2,
+        #         len(tensors) // 2 + len(tensors) % 2,
+        #         figsize=(len(tensors) * 2, 2),
+        #         dpi=dpi,
+        #     )
+        #     assert isinstance(axes, np.ndarray)
+        #     if len(tensors) == 1:
+        #         axes = [axes]  # Ensure axes is iterable for a single image
+        #     for ax, tensor in zip(axes.flatten().tolist(), tensors):
+        #         ax.imshow(
+        #             tensor.squeeze(), cmap="gray"
+        #         )  # Squeeze and display in grayscale
+        #         ax.axis("off")  # Hide axis for cleaner visualization
+        #     plt.show()
+        #
+        # show_tensors(latents)
 
         return self.mean_decoder.forward(
             latents=latents,