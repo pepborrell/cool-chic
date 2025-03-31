45db3bc36ae8926ab1359cee46fdeec694acd778
pep <borrellpep@gmail.com>
2025-03-31 18:01:57 +0200

    fix: forgot to remove some functions from delta training file

--- coolchic/delta_hypernet_train.py
+++ coolchic/delta_hypernet_train.py
@@ -5,9 +5,7 @@ from pathlib import Path
 import torch
 
 import wandb
-from coolchic.enc.component.coolchic import CoolChicEncoderOutput
-from coolchic.enc.training.loss import LossFunctionOutput, loss_function
-from coolchic.enc.utils.misc import POSSIBLE_DEVICE, get_best_device
+from coolchic.enc.utils.misc import get_best_device
 from coolchic.hypernet.hypernet import DeltaWholeNet, NOWholeNet
 from coolchic.hypernet.inference import eval_on_all_kodak, load_hypernet
 from coolchic.hypernet.training import train
@@ -28,72 +26,6 @@ def get_workdir_hypernet(config: HypernetRunConfig, config_path: Path) -> Path:
     )
     workdir.mkdir(parents=True, exist_ok=True)
     return workdir
-
-
-def get_mlp_rate(net: DeltaWholeNet) -> float:
-    rate_mlp = 0.0
-    rate_per_module = net.mean_decoder.get_network_rate()
-    for _, module_rate in rate_per_module.items():  # pyright: ignore
-        for _, param_rate in module_rate.items():  # weight, bias
-            rate_mlp += param_rate
-    return rate_mlp
-
-
-def evaluate_wholenet(
-    net: DeltaWholeNet,
-    test_data: torch.utils.data.DataLoader,
-    lmbda: float,
-    device: POSSIBLE_DEVICE,
-) -> dict[str, float]:
-    net.eval()
-    with torch.no_grad():
-        all_losses: list[LossFunctionOutput] = []
-        for test_img in test_data:
-            test_img = test_img.to(device)
-            raw_out, rate, add_data = net.forward(
-                test_img,
-                quantizer_noise_type="none",
-                quantizer_type="hardround",
-            )
-            test_out = CoolChicEncoderOutput(
-                raw_out=raw_out, rate=rate, additional_data=add_data
-            )
-            rate_mlp = get_mlp_rate(net)
-            test_loss = loss_function(
-                test_out.raw_out,
-                test_out.rate,
-                test_img,
-                lmbda=lmbda,
-                rate_mlp_bit=rate_mlp,
-                compute_logs=True,
-            )
-            all_losses.append(test_loss)
-
-        loss_tensor = torch.stack([loss.loss for loss in all_losses])  # pyright: ignore
-        avg_loss = torch.mean(loss_tensor).item()
-        std_loss = torch.std(loss_tensor).item()
-        avg_mse = torch.mean(torch.tensor([loss.mse for loss in all_losses])).item()
-        avg_psnr_db = torch.mean(
-            torch.tensor([loss.psnr_db for loss in all_losses])
-        ).item()
-        avg_total_rate_bpp = torch.mean(
-            torch.tensor([loss.total_rate_bpp for loss in all_losses])
-        ).item()
-    # Switch back to training mode.
-    net.train()
-    return {
-        "test_loss": avg_loss,
-        "test_mse": avg_mse,
-        "test_psnr_db": avg_psnr_db,
-        "test_total_rate_bpp": avg_total_rate_bpp,
-        "std_test_loss": std_loss,
-    }
-
-
-def print_gpu_info():
-    print(f"Number of GPUs available: {torch.cuda.device_count()}")
-    for i in range(torch.cuda.device_count()):
-        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
 
 
 def main():
