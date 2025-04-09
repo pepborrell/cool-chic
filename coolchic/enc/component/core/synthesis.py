384b86d486d5d8820e5b9ca7958900b1ceee0faf
pep <borrellpep@gmail.com>
2025-04-09 16:55:44 +0200

    feat: enable leakyrelu, hadn't done that before

--- coolchic/enc/component/core/synthesis.py
+++ coolchic/enc/component/core/synthesis.py
@@ -202,7 +202,7 @@ class Synthesis(nn.Module):
     possible_non_linearity = {
         "none": nn.Identity,
         "relu": nn.ReLU,
-        # "leakyrelu": nn.LeakyReLU,    # Unsupported by the decoder
+        "leakyrelu": nn.LeakyReLU,  # Unsupported by the decoder
         # "gelu": nn.GELU,              # Unsupported by the decoder
     }
 
