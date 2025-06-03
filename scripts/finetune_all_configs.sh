5af8152cd3410db77e43cd71dd63a0dfd17edd16
pep <borrellpep@gmail.com>
2025-06-03 19:03:49 +0200

    chore: automate running finetuning scripts

---
+++ scripts/finetune_all_configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dataset=clic20-pro-valid
+sbatch sbatch-files/run-finetuning.sh config_00 ${dataset}
+sbatch sbatch-files/run-finetuning.sh config_01 ${dataset}
+sbatch sbatch-files/run-finetuning.sh config_02 ${dataset}
+sbatch sbatch-files/run-finetuning.sh config_03 ${dataset}
+sbatch sbatch-files/run-finetuning.sh config_04 ${dataset}
