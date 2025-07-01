8a7f6a34522a079efc37573d052742c0019f324a
pep <borrellpep@gmail.com>
2025-07-01 21:02:55 +0200

    chore: add new config to finetune all cofigs script

--- scripts/finetune_all_configs.sh
+++ scripts/finetune_all_configs.sh
@@ -5,3 +5,4 @@ sbatch sbatch-files/run-finetuning.sh config_01 ${dataset}
 sbatch sbatch-files/run-finetuning.sh config_02 ${dataset}
 sbatch sbatch-files/run-finetuning.sh config_03 ${dataset}
 sbatch sbatch-files/run-finetuning.sh config_04 ${dataset}
+sbatch sbatch-files/run-finetuning.sh config_05 ${dataset}
